ad5d6fcf8e79f32b2f7258333320a5874d6872b4
josshuauls <jfloresch@ulasalle.edu.pe>
2024-04-24 19:58:29 -0500

    E1-Se personalizo el Toast y se agrego un EditText para personalizar el mensaje

--- Trabajo03/E1_InteraccionConImagenes/app/src/main/java/com/dares03/e1_interaccionconimagenes/MainActivity.kt
+++ Trabajo03/E1_InteraccionConImagenes/app/src/main/java/com/dares03/e1_interaccionconimagenes/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.dares03.e1_interaccionconimagenes
 
 import android.os.Bundle
+import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.Toast
 import android.widget.Toast.LENGTH_LONG
@@ -10,9 +11,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val input01 = findViewById<EditText>(R.id.input01)
+        val mensajePersonalizado = input01.text
+
         val imagen = findViewById<ImageButton>(R.id.imagenClick01)
         imagen.setOnClickListener{
-            Toast.makeText(this,"Kaká", LENGTH_LONG).show()
+            Toast.makeText(this,mensajePersonalizado, LENGTH_LONG).show()
         }
     }
 }
