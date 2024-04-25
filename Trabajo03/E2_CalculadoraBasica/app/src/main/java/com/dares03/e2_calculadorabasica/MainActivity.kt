72b65e0719feba3606141694649228c0df7b2b91
josshuauls <jfloresch@ulasalle.edu.pe>
2024-04-24 22:22:29 -0500

    E2-CalculadoraFuncional

--- Trabajo03/E2_CalculadoraBasica/app/src/main/java/com/dares03/e2_calculadorabasica/MainActivity.kt
+++ Trabajo03/E2_CalculadoraBasica/app/src/main/java/com/dares03/e2_calculadorabasica/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     var valor1 = ""
     var valor2 = ""
+    var operador = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -22,20 +23,51 @@ class MainActivity : AppCompatActivity() {
         val entrada = boton.text.toString()
         when(entrada){
             in "1234567890" -> agregarNumero(entrada)
-            in "+-*/" -> agregarOperador()
-            else -> calcularEntrada()
+            in "+-*/" -> agregarOperador(entrada)
+            "=" -> calcularEntrada()
         }
     }
 
     fun agregarNumero(digito:String){
-
+        if(valor1.isEmpty()){
+            valor1 = digito
+        }
+        else{
+            valor2 = digito
+        }
     }
 
-    fun agregarOperador(){
-
+    fun agregarOperador(operadorNuevo:String){
+        if(valor1.isNotEmpty() && valor2.isNotEmpty()){
+            return
+        }
+        operador = operadorNuevo
+        actualizarPantallaEntrada()
     }
 
     fun calcularEntrada(){
+        if(valor1.isNotEmpty() && valor2.isNotEmpty()){
+            val resultado = when(operador){
+                "+" -> valor1.toDouble() + valor2.toDouble()
+                "-" -> valor1.toDouble() + valor2.toDouble()
+                "*" -> valor1.toDouble() + valor2.toDouble()
+                "/" -> valor1.toDouble() + valor2.toDouble()
+                else -> 0.0
+            }
+            actualizarPantallaSalida(resultado.toString())
+            valor1 = ""
+            valor2 = ""
+            operador = ""
+        }
+    }
 
+    fun actualizarPantallaEntrada(){
+        val pantallaEntrada = findViewById<TextView>(R.id.pantallaEntrada)
+        pantallaEntrada.text = "$valor1 $operador $valor2"
+    }
+
+    fun actualizarPantallaSalida(resultado:String){
+        val pantallaSalida = findViewById<TextView>(R.id.pantallaSalida)
+        pantallaSalida.text = resultado
     }
 }
