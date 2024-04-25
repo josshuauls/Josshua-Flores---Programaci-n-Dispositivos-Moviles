414cceb6187faf3505392aa1156a48376c6f6693
josshuauls <jfloresch@ulasalle.edu.pe>
2024-04-24 21:22:41 -0500

    E2-Avance funcion

--- Trabajo03/E2_CalculadoraBasica/app/src/main/java/com/dares03/e2_calculadorabasica/MainActivity.kt
+++ Trabajo03/E2_CalculadoraBasica/app/src/main/java/com/dares03/e2_calculadorabasica/MainActivity.kt
@@ -1,20 +1,41 @@
 package com.dares03.e2_calculadorabasica
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    var valor1 = ""
+    var valor2 = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+    }
+
+    fun click(vista:View){
+        val boton = vista as Button
+        val entrada = boton.text.toString()
+        when(entrada){
+            in "1234567890" -> agregarNumero(entrada)
+            in "+-*/" -> agregarOperador()
+            else -> calcularEntrada()
         }
+    }
+
+    fun agregarNumero(digito:String){
+
+    }
+
+    fun agregarOperador(){
+
+    }
+
+    fun calcularEntrada(){
+
     }
 }
